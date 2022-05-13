79c4030ee645abb45512dc71b4cb765508892c0f
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-05-13 13:50:18 -0500

    Added a "Bonus" input box that will be automatically added on button click

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Use Math.random() * d8 # and then
 Math.floor() to turn the result into an integer */
 function roll(number) {
-    let result = Math.random() * number + 1;
+    let result = Math.random() * number + 1 + parseFloat(bonus.value);
     result = Math.floor(result);
     document.getElementById("resultDisplay").innerHTML = result;
 }

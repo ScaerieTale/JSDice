fdc3bf8b5ca517d3fc6212b0d349a02bdcc5ceec
ScaerieTale <scaerietale@gmail.com>
2024-04-21 02:01:09 -0500

    Add rollMult(num1, num2) function (partially working)

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ function roll(number) {
     bonus.value = 0;
 }
 
+function rollMult(num1, num2) {
+    let result = 0;
+    let rolls = [];
+    for (let i = 0; i < num1; i++) {
+        rolls.push(Math.random() * num2 + 1);
+    }
+    for (let j = 0; j < rolls.length; j++) {
+        result += rolls[j];
+    }
+    document.getElementById("resultDisplay").innerHTML = Math.floor(result);
+    // TODO: add bonus.value (after finding the issue with inconsistent roll result)
+}

8eb94bf0be817bab174aaefb56933c591f1ebd25
ScaerieTale <scaerietale@gmail.com>
2024-04-20 23:33:51 -0500

    Change background color to black Center text inside Bonus tab

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ function roll(number) {
     let result = Math.random() * number + 1 + parseFloat(bonus.value);
     result = Math.floor(result);
     document.getElementById("resultDisplay").innerHTML = result;
+    bonus.value = 0;
 }
 

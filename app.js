02874faa0c3c13d3ff014c29117cd7d907835b9b
ScaerieTale <scaerietale@gmail.com>
2024-04-21 07:34:39 -0500

    Add toggleTheme() to switch between light and dark modes! Style up a new L:ight Mode Add button to top left to switch modes

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 let result = 0;
 bonus.value = 0;
 
+// Generates a random number bwetween num1 (the numberr of dice to throw) and num2 (the value of the di)
 function rollDice(num1, num2) {
     for (let i = 0; i < num1; i++) {
         result += Math.floor(Math.random() * num2) + 1;
     }
 }
 
-
+// calls rollDice(), inserting the number of dice (num1) and value (num2)
+// and then displays the output in the results display container
 function displayRollResults(num1, num2) {
     rollDice(num1, num2) + bonus.value;
     document.getElementById("resultDisplay").innerHTML = result;
     result = 0;
 }
+
+// switch between Light and Dark mode
+function toggleTheme() { 
+    let theme = document.getElementsByTagName('link')[0];
+    let img = document.getElementById("theme-img");
+
+    if (theme.getAttribute('href') == 'light-mode.css') { 
+        theme.setAttribute('href', 'dark-mode.css');
+        img.setAttribute('src', 'light.png');
+    } else { 
+            theme.setAttribute('href', 'light-mode.css');
+        img.setAttribute('src', 'dark.png');
+
+    }
+}
+     

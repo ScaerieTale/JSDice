1fc3e6af8aa374d54b362ff8e22b3e992bb842c8
ScaerieTale <scaerietale@gmail.com>
2024-04-21 03:10:54 -0500

    BUGFIX: Fixed longstanding 2d6 occasionally displaying 1 - HUGE thanks to Sam Minnée for the help with the bug fix! Code refactor to split roll and results display into separate functions - This is partly to fix the above bug, and to prepare for phase 2 adding custom dice

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-/* Use Math.random() * d8 # and then
-Math.floor() to turn the result into an integer */
-function roll(number) {
-    let result = Math.random() * number + 1 + parseFloat(bonus.value);
-    result = Math.floor(result);
-    document.getElementById("resultDisplay").innerHTML = result;
+let result = 0;
+bonus.value = 0;
+
+function rollDice(num1, num2) {
+    for (let i = 0; i < num1; i++) {
+        result += Math.floor(Math.random() * num2) + 1;
+    }
 }
 
+
+function displayRollResults(num1, num2) {
+    rollDice(num1, num2) + bonus.value;
+    document.getElementById("resultDisplay").innerHTML = result;
+    result = 0;
+}
